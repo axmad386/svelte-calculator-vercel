5883bb04bca13c4881ef3baaa113eeec068a6379
Akhmad Salafudin <axmad386@gmail.com>
2021-04-28 15:33:29 +0700

    chore: add svelte adapter static

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,5 +1,5 @@
 const preprocess = require('svelte-preprocess');
-
+const adapter = require('@sveltejs/adapter-static')
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -8,6 +8,7 @@ module.exports = {
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: adapter()
 	}
 };
